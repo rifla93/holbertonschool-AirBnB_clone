545089bc9c8695a5c3c1df57cb5653ed337cc428
MDrenova <mateodrenova@gmail.com>
2024-02-26 14:57:31 +0100

    Update: constructor, id of object outside conditions

--- models/base_model.py
+++ models/base_model.py
@@ -9,15 +9,14 @@ class BaseModel:
     """class Base"""
 
     def __init__(self, *args, **kwargs):
-        
+        self.id = str(uuid4())
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key == "created_at" or key == "updated_at":
-                        datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                        datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                         setattr(self, key, value)
         else:
-            self.id = str(uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
 
@@ -33,18 +32,3 @@ class BaseModel:
         dict_copy["created_at"] = self.created_at.isoformat()
         dict_copy["updated_at"] = self.updated_at.isoformat()
         return dict_copy
-
-
-
-
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
