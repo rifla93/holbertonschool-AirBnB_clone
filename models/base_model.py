b87f3eb2bfe86a4a8fc543ac57b5ebe73cf65754
MDrenova <mateodrenova@gmail.com>
2024-02-26 13:45:00 +0100

    Create directories models and create class base

---
+++ models/base_model.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from uuid import uuid4
+import datetime
+
+"""Base model """
+
+
+class BaseModel:
+    """class Base"""
+
+    def __init__(self):
+        self.id = str(uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        dict_copy = self.__dict__.copy()
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        return dict_copy
